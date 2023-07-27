5ae0cef79c2168d2b65bc4a4ff2d71dfc32dd5b5
omid <omidpoorali1992@gmail.com>
2023-07-27 16:12:45 +0330

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+// maps to root `/` URL
+ 
+import variables from '@/styles/variables.module.scss'
+ 
+export default function Page() {
+  return <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+}
