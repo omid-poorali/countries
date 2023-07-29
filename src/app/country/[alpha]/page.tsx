5ac5dcae694b5d9484cc139a493d850a3397a63a
omid <omidpoorali1992@gmail.com>
2023-07-29 20:03:40 +0330

    feat: country styles

--- src/app/country/[alpha]/page.tsx
+++ src/app/country/[alpha]/page.tsx
@@ -7,6 +7,7 @@ import * as Models from '@/models';
 import * as Utils from '@/utils';
 import classes from './page.module.scss';
 import { Button } from '@/components';
+import clsx from 'clsx';
 
 
 type Country = Omit<Models.Country, "borders"> & {
@@ -48,12 +49,17 @@ export default async function Page({ params }: { params: { alpha: string } }) {
 
 
   return (
-    <div className={classes.root}>
-      <div className={classes.flagSection}>
+    <div className={clsx('container',classes.root)}>
+
+      <div className={classes.top}>
         <Link href={Utils.Route.generatePath(Routes.COUNTRIES)} passHref legacyBehavior>
           <Button>Back</Button>
         </Link>
-        <div className={classes.flagWrapper}>
+      </div>
+
+      <div className={classes.flag}>
+
+        <div className={classes.imageWrapper}>
           <Image
             src={country.flags.png}
             quality={100}
@@ -66,40 +72,44 @@ export default async function Page({ params }: { params: { alpha: string } }) {
           />
         </div>
       </div>
-      <div className={classes.info}>
-        <h2>{country.name}</h2>
-        <div className={classes.section}>
-          {React.Children.toArray(['nativeName', "population", "region", "subregion", "capital"].map(property => (
-            <div className={classes.property}>
-              <h3>{propertyTitle[property]}:</h3>
-              <span>{country[property as CountryProperty]}</span>
-            </div>
-          )))}
-        </div>
-        <div className={classes.section}>
-          <div className={classes.property}>
-            <h3>Top Level Domain:</h3>
-            <span>{country.topLevelDomain}</span>
-          </div>
-          <div className={classes.property}>
-            <h3>Currencies:</h3>
-            <span>{country.currencies.map(currency => currency.symbol).toString()}</span>
-          </div>
-          <div className={classes.property}>
-            <h3>Languages:</h3>
-            <span>{country.languages.map(lan => lan.name).toString()}</span>
-          </div>
-        </div>
 
-        <div className={classes.borderCountries}>
-          <h3>Border Countries:</h3>
-          {React.Children.toArray(country.borders.map(border => (
-            <Link href={Utils.Route.generatePath(Routes.COUNTRY, { alpha: border.alpha })} passHref legacyBehavior>
-              <Button>{border.name}</Button>
-            </Link>
-          )))}
+      <div className={classes.left}>
+        <h2>{country.name}</h2>
+
+        {React.Children.toArray(['nativeName', "population", "region", "subregion", "capital"].map(property => (
+          <div className={classes.property}>
+            <strong>{propertyTitle[property]}:</strong>
+            <span>{country[property as CountryProperty]}</span>
+          </div>
+        )))}
+
+      </div>
+
+
+      <div className={classes.right}>
+        <div className={classes.property}>
+          <strong>Top Level Domain:</strong>
+          <span>{country.topLevelDomain}</span>
+        </div>
+        <div className={classes.property}>
+          <strong>Currencies:</strong>
+          <span>{country.currencies.map(currency => currency.symbol).toString()}</span>
+        </div>
+        <div className={classes.property}>
+          <strong>Languages:</strong>
+          <span>{country.languages.map(lan => lan.name).toString()}</span>
         </div>
       </div>
+
+      <div className={classes.bottom}>
+        <strong>Border Countries:</strong>
+        {React.Children.toArray(country.borders.map(border => (
+          <Link href={Utils.Route.generatePath(Routes.COUNTRY, { alpha: border.alpha })} passHref legacyBehavior>
+            <Button>{border.name}</Button>
+          </Link>
+        )))}
+      </div>
+
     </div>
   )
 
